37b34f2cc44aa26f24ff405e4d99f104ba2e773c
chris pryer <christophpryer@gmail.com>
2016-10-08 16:06:11 -0400

    started testing concepts for dynamic storage

--- js/supfeech.js
+++ js/supfeech.js
@@ -9,20 +9,22 @@ var config = {
 firebase.initializeApp(config);
 
 //static set for first message in chatbox
-var text0 = document.getElementById("text0");
-var realDb0 = firebase.database().ref().child('message');
+var text0 = document.getElementById('text0');
+var realDb0 = firebase.database().ref('chat').child(0);
 realDb0.on('value', snap => text0.innerText = snap.val());
 
 //function for writing data to firebase database
 function postMessage(){
   //sets new branch to chat and adds message1 with newText value
-  var newText = document.getElementById("usermsg").value;
+  var oldText = document.getElementById('text0').innerText;
+  var newText = document.getElementById('usermsg').value;
   firebase.database().ref('chat').set({
-  message1: newText
-})
+  0: oldText,
+  1: newText
+});
 
   //inserts new message into second p element within chatbox (static)
   var text1 = document.getElementById("text1");
-  var realDb1 = firebase.database().ref('chat').child('message1');
+  var realDb1 = firebase.database().ref('chat').child(1);
   realDb1.on('value', snap => text1.innerText = snap.val());
 }
