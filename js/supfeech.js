ed975806912cf67eecabad2ad8ad133e5b943594
chris pryer <christophpryer@gmail.com>
2016-10-08 15:21:40 -0400

    app structure update

---
+++ js/supfeech.js
@@ -0,0 +1,28 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA19nR6eSsDHRIewBs1WAjyGs5NCCKEvFs",
+  authDomain: "supfeech.firebaseapp.com",
+  databaseURL: "https://supfeech.firebaseio.com",
+  storageBucket: "supfeech.appspot.com",
+  messagingSenderId: "355684958639"
+  };
+firebase.initializeApp(config);
+
+//static set for first message in chatbox
+var text0 = document.getElementById("text0");
+var realDb0 = firebase.database().ref().child('message');
+realDb0.on('value', snap => text0.innerText = snap.val());
+
+//function for writing data to firebase database
+function postMessage(){
+  //sets new branch to chat and adds message1 with newText value
+  var newText = document.getElementById("usermsg").value;
+  firebase.database().ref('chat').set({
+  message1: newText
+})
+
+  //inserts new message into second p element within chatbox (static)
+  var text1 = document.getElementById("text1");
+  var realDb1 = firebase.database().ref('chat').child('message1');
+  realDb1.on('value', snap => text1.innerText = snap.val());
+}
